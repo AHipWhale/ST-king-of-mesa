ef7994775877f838a4ef6c7dd9a299db08133425
AHipWhale <charlie.choffat@gmail.com>
2020-11-24 16:13:20 +0100

    batch run

--- Tutorial/money_model.py
+++ Tutorial/money_model.py
@@ -2,6 +2,7 @@ from mesa import Agent, Model
 from mesa.time import RandomActivation
 from mesa.space import MultiGrid
 from mesa.datacollection import DataCollector
+from mesa.batchrunner import BatchRunner
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -45,6 +46,7 @@ class MoneyModel(Model):
         self.num_agents = N
         self.grid = MultiGrid(width, height, True)
         self.schedule = RandomActivation(self)
+        self.running = True
 
         # Create agents
         for i in range(self.num_agents):
@@ -81,4 +83,21 @@ plt.show()
 
 one_agent_wealth = agent_wealth.xs(14, level="AgentID")
 one_agent_wealth.Wealth.plot()
+plt.show()
+
+fixed_params = {"width": 10,
+               "height": 10}
+variable_params = {"N": range(10, 500, 10)}
+
+batch_run = BatchRunner(MoneyModel,
+                        variable_params,
+                        fixed_params,
+                        iterations=5,
+                        max_steps=100,
+                        model_reporters={"Gini": compute_gini})
+batch_run.run_all()
+
+run_data = batch_run.get_model_vars_dataframe()
+run_data.head()
+plt.scatter(run_data.N, run_data.Gini)
 plt.show()
